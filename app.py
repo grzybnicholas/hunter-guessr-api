e2bd84c43aa1cc0fd2bee21aa574dc791059724d
grzybnicholas <92946140+grzybnicholas@users.noreply.github.com>
2024-10-15 21:22:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,6 @@ try:
         passwd = "f458826d",
         database = "heroku_4137fb28e4a76f3",
         ssl_disabled=True
-        pool_name="mypool",
-        pool_size=5
     )
 
     MyCursor = MyDB.cursor()
